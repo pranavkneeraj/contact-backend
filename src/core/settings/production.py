b458ad4d97bd211d1c6965b18874788759ec86db
Pranav <pranav.k@amazatic.com>
2017-06-20 20:46:47 +0530

    db conf for production

--- src/core/settings/production.py
+++ src/core/settings/production.py
@@ -10,7 +10,22 @@ SECRET_KEY = os.environ.get(
     "SECRET_KEY", 'iw7&5xlpe_*jk*vd=0z4hs#o=kg1g9ze7xx@528o3tq!(h!qx$')
 
 DEFAULT_CONNECTION = dj_database_url.parse(os.environ.get("DATABASE_URL"))
-DATABASES = {"default": DEFAULT_CONNECTION}
+DATABASES = database_configs(databases_dict={
+    'unsharded_databases': [
+    ],
+    'sharded_databases': [
+        {
+            'name': 'default',
+            'environment_variable': 'DATABASE_URL',
+            'default_database_url':  'postgres://admin:admin@localhost:5432/contact1'
+        },
+        {
+            'name': 'contact2',
+            'environment_variable': 'HEROKU_POSTGRESQL_SILVER_URL',
+            'default_database_url': 'postgres://admin:admin@localhost:5432/contact2'
+        },
+    ]
+})
 DEBUG = True if (os.environ.get("DEBUG", "true").lower() == "true") else False
 ALLOWED_HOSTS = json.loads(os.environ.get("ALLOWED_HOSTS", "[\"*\"]"))
 
